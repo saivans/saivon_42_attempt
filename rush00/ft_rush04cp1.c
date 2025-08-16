9d92da961fb76c3ba6c5bb274af1e9425d09eae7
Saif Tagma <stagma@c2r2p10.1337.ma>
2025-08-16 15:18:30 +0100

    add

---
+++ rush00/ft_rush04cp1.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rush04cp1.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stagma <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/16 14:58:50 by stagma            #+#    #+#             */
+/*   Updated: 2025/08/16 15:15:53 by stagma           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_firstlast(char a, char c, int i, int y);
+
+void	ft_mid(int i, int j, int x, int y);
+
+void	ft_putchar(char c);
+
+void	ft_rush04cp1(int x, int y)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0 ;
+	j = 1 ;
+	k = 0 ;
+	if (x == 0 || y == 0)
+		return ;
+	while (k < y)
+	{
+		if (k == 0)
+			ft_firstlast('A', 'C', i, y);
+		else if (k == 1)
+			ft_mid(i, j, x, y);
+		else if (k == (y - 1))
+		{
+			ft_putchar('\n');
+			ft_firstlast('C', 'A', i, y);
+		}
+		k++;
+	}
+	ft_putchar('\n');
+}
+
+void	ft_firstlast(char a, char c, int i, int y)
+{
+	ft_putchar(a);
+	while (i < y - 2)
+	{
+		ft_putchar('B');
+		i++;
+	}
+	i = 0;
+	if (i < y - 2)
+	{
+		ft_putchar(c);
+	}
+}
+
+void	ft_mid(int i, int j, int x, int y)
+{
+	while (j < y - 1)
+	{
+		ft_putchar('\n');
+		ft_putchar('B');
+		i = 0 ;
+		while (i < x - 2)
+		{
+			ft_putchar(' ');
+			i++;
+		}
+		i = 0;
+		if (i < x - 2)
+		{
+			ft_putchar('B');
+			j++;
+		}
+	}
+}
